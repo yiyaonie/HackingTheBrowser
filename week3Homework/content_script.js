7422862a4571170e1d4abcc5529d892ff7ca76a0
yiyao <nieyiyao@gmail.com>
2019-03-04 21:21:32 -0500

    update

--- week3Homework/content_script.js
+++ week3Homework/content_script.js
@@ -1,25 +1,27 @@
-chrome.storage.onChanged.addListener(function(changes) {
-    var action = changes['action'];
-    if(action.newValue === 'style') {
-        // YOUR CODE HERE
-        console.log("change style");
-        let allH = document.querySelectorAll("h1");
-        allH.appendChild("Happy New Year!");
-        allH.style.color = "red";
+chrome.runtime.onMessage.addListener(function(message) {
+  /// message == "whatever"
+  if(message == "style"){
+    console.log("change style");
+    $("h1").text("新年快乐 Happy New Year!")
+    $("h1").css('color','red');
 
-        let allH2 = document.querySelectorAll("h2");
-        allH2.appendChild("🐷🎎");
-        allH2.style.color = "red";
-    }
+    $("h2").text("猪年快乐 🐷🎎")
+    $("h2").css('color','red');
 
-    if(action.newValue === 'celebrate') {
-        // YOUR CODE HERE
-        console.log("celebrate");
-        var celebrate1 = document.createElement("img");
-        celebrate1.src = "images/celebrate1.gif";
+    // $("body").css('background-color','red');
+  }
+  if(message == "celebrate"){
+    console.log("celebrate");
+    var imgURL = chrome.extension.getURL("images/celebrate1.gif");
+    var imgURL2 = chrome.extension.getURL("images/celebrate2.gif");
 
-        var celebrate2 = document.createElement("img");
-        celebrate2.src = "images/celebrate2.gif";
-    }
+    $("<img alt='celebrate' src = 'chrome-extension://mbnknpamglcaepfcgcjklcoaalbgmnil/images/celebrate1.gif'>").appendTo("body").addClass("celebrate1");
+    document.getElementsByClassName("celebrate1").src = imgURL;
 
+    $("<img alt='celebrate' src = 'chrome-extension://mbnknpamglcaepfcgcjklcoaalbgmnil/images/celebrate2.gif'>").appendTo("body").addClass("celebrate2");
+    document.getElementsByClassName("celebrate2").src = imgURL2;
+
+    $(".celebrate1").css({'position':'fixed','bottom':'0','left':'0','height':'200px','width':'auto'});
+    $(".celebrate2").css({'position':'fixed','bottom':'0','right':'0','height':'200px','width':'auto'});
+  }
 });
--- week3Homework/popup.js
+++ week3Homework/popup.js
@@ -1,23 +1,24 @@
 let changeStyle = document.getElementById("style");
 let celebrate = document.getElementById("celebrate");
-let song = document.getElementById("song");
 
 function play(){
       var audio = document.getElementById("audio");
       audio.play();
 }
-song.onclick = play;
 
 document.getElementById('style').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-        chrome.storage.local.set({ action: 'style' });
-        console.log("send style");
+      console.log("click style popup")
+      var tab = activeTabs[0];
+      chrome.tabs.sendMessage(tab.id, "style");
     });
 });
 
 document.getElementById('celebrate').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-        chrome.storage.local.set({ action: 'celebrate' });
-        console.log("send celebrate");
+      console.log("click celebrate popup");
+      var tab = activeTabs[0];
+      chrome.tabs.sendMessage(tab.id, "celebrate");
     });
+    play();
 });
